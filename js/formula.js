059e1988b1769ce8147209df41d4653240f3bff0
Mark Praprotnik <emarkymark@gmail.com>
2016-11-28 22:26:02 -0600

    Readd shift() to equal pays and receives in math calculations, add test cases

--- js/formula.js
+++ js/formula.js
@@ -207,13 +207,13 @@ $(document).ready(function() {
 			}
 
 			// If payer owes zero, debts were assumed during merge. remove pay amount
-			else if ($pays[0] !== undefined && $pays[0].debt == 0) {
+			if ($pays[0] !== undefined && $pays[0].debt == 0) {
 				var $payerZero = '<div>' + $pays[0].name + "'s debts were cancelled out for " + $pays[0].items.join("\, ") + '</div>';
 				$results.append($payerZero);
 				$pays.shift();
 			}
 			// If receiver receives zero, remove receive amount
-			else if ($receives[0] !== undefined && $receives[0].debt == 0) {
+			if ($receives[0] !== undefined && $receives[0].debt == 0) {
 				var $receiverZero = '<div>' + $receives[0].name + "'s debts were cancelled out for " + $receives[0].items.join("\, ") + '</div>';
 				$results.append($receiverZero);
 				$receives.shift();
@@ -227,7 +227,7 @@ $(document).ready(function() {
 				$receives.shift();
 			}
 			// If receiver receives more money than payer owes, adjust new receive amount and zero out pay amount, remove pay amount from array 
-			else if ($pays[0].debt < $receives[0].debt) {
+			if ($pays[0].debt < $receives[0].debt) {
 				var $receiverMore = '<div>' + $pays[0].name + ' owes ' + $receives[0].name + ' ' + formatCurrency($pays[0].debt) + ' for ' + $pays[0].items.join("\, ") + '</div>';
 				$results.append($receiverMore);
 				$receives[0].debt -= $pays[0].debt;
@@ -235,11 +235,13 @@ $(document).ready(function() {
 				$pays.shift();
 			}
 			// If payer owes the same amount that the receiver receives, zero out both receive and pay amounts, remove pay and receive amounts
-			else if (($pays[0].debt === $receives[0].debt) && (($pays[0].debt > 0) || ($receives[0].debt > 0))) {
+			if (($pays[0].debt === $receives[0].debt) && (($pays[0].debt > 0) || ($receives[0].debt > 0))) {
 				var $payerEqualReceiver = '<div>' + $pays[0].name + ' owes ' + $receives[0].name + ' ' + formatCurrency($receives[0].debt) + ' for ' + $pays[0].items.join("\, ") + '</div>';
 				$results.append($payerEqualReceiver);
 				$pays[0].debt -= $pays[0].debt;
 				$receives[0].debt -= $receives[0].debt;
+				$pays.shift();
+				$receives.shift();
 			}
 		}	
 
